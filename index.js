72ecc0f023127005a5874c48c78ac1a13114132b
danny <47242861+contrafactum@users.noreply.github.com>
2021-03-03 09:17:41 +0000

    Updated index.js
    Added Code Snippet 2 with fixes. Tested with Node.JS.
    Works just fine.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+//Code snippet 1
 let string = "hello";
-
 string = string + "world";
-
 console.log(string);
+
+//Code snippet 2
+let time;
+function setTime() {
+    time = new Date();
+    }
+setTime();
+console.log(time)
